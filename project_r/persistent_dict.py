be19fac18e12279f44eceff49d541be3384e63f8
douglascdev <douglasc.dev@gmail.com>
2024-01-04 19:34:48 -0300

    Tests

--- project_r/persistent_dict.py
+++ project_r/persistent_dict.py
@@ -2,42 +2,58 @@ import json
 import logging
 from io import TextIOBase
 from pathlib import Path
-from typing import Any
+from typing import TYPE_CHECKING, Any, Generic, Type, TypedDict, TypeVar
 
 from project_r.decorators import ensure_file_is_open
 from project_r.rwlock import RWLock
 
 __all__ = ["PersistentDict"]
 
+T = TypeVar("T")
+# T = TypeVar("T", bound=TypedDict)
 
-class PersistentDict:
+
+class PersistentDict(Generic[T]):
     """
     Persistent async key-value storage.
+
+    Generic type T is an optional TypedDict you can write that enables type hints.
+
+    Filecan be a Path to a database file or a TextIOBase if you don't want to use a file,
+    such as using a StringIO object for a memory database.
+    A metadata json file will be created automatically in the same directory with the same
+    name, but removing the extension and appending "_metadata.json".
+
+    Example:
+
+    ```python
+    class MyData(TypedDict):
+        name: str
+
+    storage = PersistentDict[MyData](file=Path("/path/to/file.aks"))
+    # Since we are using a TypedDict, your IDE will complete name as a string
+    name = await storage.get("name")
+
+    # This would work, but without providing type hints:
+    storage = PersistentDict(file=Path("/path/to/file.aks"))
+    ```
     """
 
     def __init__(self, file: TextIOBase | Path) -> None:
-        """
-        File can be a Path to a database file or a TextIOBase if you don't want to use a file,
-        such as using a StringIO object for a memory database.
-
-        A metadata json file will be created automatically in the same directory with the same
-        name, but removing the extension and appending "_metadata.json".
-        """
-
         self._rwlock = RWLock()
         self._file: TextIOBase = self._get_file_object(file)
 
         try:
-            self._data: dict = json.load(self._file)
+            self._data: T = json.load(self._file)
         except json.JSONDecodeError:
             # File is not empty, database exists but failed to load
             if self._file.read():
                 raise Exception("Failed to load database.")
 
             # File is empty so it's a new database, make an empty dict
-            self._data: dict = {}
+            self._data: T = {}
 
-        assert type(self._data) is dict, "Database file is not a dictionary"
+        # assert isinstance(self._data, dict), "Database file is not a dictionary"
 
     def _get_file_object(self, file) -> TextIOBase:
         if type(file) is Path:
@@ -48,10 +64,10 @@ class PersistentDict:
             raise Exception("Invalid file type")
 
     @ensure_file_is_open
-    async def get(self, key) -> Any:
+    async def get(self, key: str) -> T[str, Any]:
         async with self._rwlock.reader_locked():
-            result = self._data.get(key, None)
-            return result
+            # result =
+            return self._data[key]
 
     @ensure_file_is_open
     async def set(self, key, value):
--- tests/test_persitent_dict.py
+++ tests/test_persitent_dict.py
@@ -1,17 +1,31 @@
 import unittest
 from io import StringIO
+from pathlib import Path
+from typing import TypedDict
 
 from project_r import DatabaseClosedError, PersistentDict
+
+
+class MyData(TypedDict):
+    asd: str
+    age: int
 
 
 class PersistentDictBasicTestCase(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         self.buffer = StringIO("")
-        self.storage = PersistentDict(self.buffer)
+        self.storage = PersistentDict[MyData](self.buffer)
 
     async def test_get_set_remove(self):
+        storage = self.storage
+        await storage.set("name", "ASD")
+        asd = await storage.get("asd")
+        storage._data["asd"]
+        # name.
+
         await self.storage.set("asd", "asd")
         self.assertEqual(await self.storage.get("asd"), "asd")
+        asd = await self.storage.get("asd")
         await self.storage.remove("asd")
         self.assertIs(await self.storage.get("asd"), None)
 
